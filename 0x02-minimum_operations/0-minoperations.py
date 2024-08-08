f70e0b4a26ad2fadb02e4edb36a2283e02a5424b
Jumoke1 <kazeemjumoke12@gmail.com>
2024-08-09 00:29:24 +0100

    starwars

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,7 +10,8 @@ def minOperations(n):
     operations = 0
     factor = 2
 
-    while n > 1:
+    while n > 1:.
+    
         while n % factor == 0:
             operations += factor
             n //= factor
